d9d7de4c9c68838a81b5e7432e6a5a6ab9e41d77
Dustin Schau <dustinschau@gmail.com>
2019-08-19 12:42:59 -0700

    chore: use next version

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,7 @@ module.exports = {
       },
     },
     {
-      resolve: '@dschau/gatsby-source-cosmicjs',
+      resolve: 'gatsby-source-cosmicjs',
       options: {
         bucketSlug: process.env.COSMIC_BUCKET || 'gatsby-blog-cosmic-js',
         objectTypes: ['posts','settings'],
